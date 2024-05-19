78fc0b204b9abddaf723f7d50f4dde852b8fb58b
deniskorbakov <korbakovd@gmail.com>
2024-05-19 23:13:09 +0300

    feat: add new enums

--- cmd/connect/enums.go
+++ cmd/connect/enums.go
@@ -6,3 +6,37 @@ const (
 	LongDescription  = `Данная команда выводит список ваших добавленных соедениний по ssh,
 в случае его отсутвия попросит добавить подключения`
 )
+
+const (
+	FilterPlaceholder = "Введите или выбирите название подключения"
+	SelectionPrompt   = "Выбор подключения"
+	FilterPrompt      = "Поиск по алиуса:"
+	PageSize          = "5"
+)
+
+const (
+	Template = `
+{{- if .Prompt -}}
+	{{ Bold .Prompt }}
+{{ end -}}
+
+{{ if .IsFiltered }}
+	{{- print .FilterPrompt " " .FilterInput }}
+{{ end }}
+
+{{- range  $i, $choice := .Choices }}
+	{{- if IsScrollUpHintPosition $i }}
+		{{- print "⇡ " -}}
+	{{- else if IsScrollDownHintPosition $i -}}
+		{{- print "⇣ " -}}
+	{{- else -}}
+		{{- print "  " -}}
+	{{- end -}}
+	
+	{{- if eq $.SelectedIndex $i }}
+		{{- print (Foreground "201" (Bold "▸ ")) (Selected $choice) "\n" }}
+	{{- else }}
+		{{- print " " (Unselected $choice) "\n" }}
+	{{- end }}
+{{- end}}`
+)
--- cmd/delete/enums.go
+++ cmd/delete/enums.go
@@ -5,3 +5,37 @@ const (
 	ShortDescription = `Удаление подключение по алиасу`
 	LongDescription  = `Данная команда позволяет удалить подключение по алиасу`
 )
+
+const (
+	FilterPlaceholder = "Введите или выбирите название для удаление подключения"
+	SelectionPrompt   = "Выбор удаление подключения"
+	FilterPrompt      = "Поиск по алиуса:"
+	PageSize          = "5"
+)
+
+const (
+	Template = `
+{{- if .Prompt -}}
+	{{ Bold .Prompt }}
+{{ end -}}
+
+{{ if .IsFiltered }}
+	{{- print .FilterPrompt " " .FilterInput }}
+{{ end }}
+
+{{- range  $i, $choice := .Choices }}
+	{{- if IsScrollUpHintPosition $i }}
+		{{- print "⇡ " -}}
+	{{- else if IsScrollDownHintPosition $i -}}
+		{{- print "⇣ " -}}
+	{{- else -}}
+		{{- print "  " -}}
+	{{- end -}}
+	
+	{{- if eq $.SelectedIndex $i }}
+		{{- print (Foreground "201" (Bold "× ")) (Selected $choice) "\n" }}
+	{{- else }}
+		{{- print " " (Unselected $choice) "\n" }}
+	{{- end }}
+{{- end}}`
+)
