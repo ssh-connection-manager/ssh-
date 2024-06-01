ced4a0864714ff825c4f7e5bffc03bd4c78bbb2b
deniskorbakov <korbakovd@gmail.com>
2024-06-01 21:09:17 +0300

    fix: put extra logic in crypto file

--- app/crypt/decrypt.go
+++ app/crypt/decrypt.go
@@ -1,34 +1,14 @@
 package crypt
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
+	"ssh+/app/output"
 )
 
-func decrypt(key []byte, ciphertext []byte) []byte {
-	block, err := aes.NewCipher(key)
+func Decrypt(str string) string {
+	decrypted, err := decrypt(GetKey(), str)
 	if err != nil {
-		panic(err)
+		output.GetOutError("Ошибка шифрование")
 	}
 
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err)
-	}
-
-	nonceSize := aesGCM.NonceSize()
-
-	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
-	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	return plaintext
-}
-
-func Decrypt(ciphertext string) string {
-	decrypted := decrypt(GetKey(), []byte(ciphertext))
-
-	return string(decrypted)
+	return decrypted
 }
--- app/crypt/encrypt.go
+++ app/crypt/encrypt.go
@@ -1,35 +1,12 @@
 package crypt
 
-import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"io"
-)
-
-func encrypt(key []byte, plaintext []byte) []byte {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err)
-	}
-
-	nonce := make([]byte, aesGCM.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err)
-	}
-
-	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
-	return ciphertext
-}
+import "ssh+/app/output"
 
 func Encrypt(str string) string {
-	plaintext := []byte(str)
-	ciphertext := encrypt(GetKey(), plaintext)
+	ciphertext, err := encrypt(GetKey(), str)
+	if err != nil {
+		output.GetOutError("Ошибка шифрование")
+	}
 
-	return string(ciphertext)
+	return ciphertext
 }
