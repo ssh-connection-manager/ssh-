f9955a1660dc14f105e017cc5e86f1b23c50d2a1
deniskorbakov <korbakovd@gmail.com>
2024-06-01 17:08:35 +0300

    fix: delete extra code

--- app/file/helper.go
+++ app/file/helper.go
@@ -6,34 +6,6 @@ import (
 	"ssh+/app/output"
 )
 
-func (c *Connections) GetConnectionsAlias() []string {
-	filePath, err := GetFullPath(os.Getenv("FILE_NAME_CONNECTS"))
-	if err != nil {
-		output.GetOutError("Ошибка получения путя к файлу")
-	}
-
-	c.SerializationJson(ReadFile(filePath))
-
-	var result []string
-
-	for _, conn := range c.Connects {
-		result = append(result, conn.Alias)
-	}
-
-	if len(result) == 0 {
-		output.GetOutError("Подключений не найдено")
-	}
-
-	return result
-}
-
-func (c *Connections) deleteJsonDataByIndex(index int) {
-	copy(c.Connects[index:], c.Connects[index+1:])
-
-	c.Connects[len(c.Connects)-1] = Connect{}
-	c.Connects = c.Connects[:len(c.Connects)-1]
-}
-
 func getPathToFile() string {
 	return os.Getenv("PATH_FILE") + os.Getenv("SEPARATOR")
 }
