6a98779a0741f4f4f780ec3cd27402185b534e12
deniskorbakov <korbakovd@gmail.com>
2024-06-01 19:50:19 +0300

    fix: delete extra code

--- app/json/json.go
+++ app/json/json.go
@@ -2,22 +2,8 @@ package json
 
 import (
 	"encoding/json"
-	"os"
-
-	"ssh+/app/file"
 	"ssh+/app/output"
 )
-
-type Connections struct {
-	Connects []Connect `json:"connects"`
-}
-
-type Connect struct {
-	Alias    string `json:"alias"`
-	Login    string `json:"login"`
-	Address  string `json:"address"`
-	Password string `json:"password"`
-}
 
 func (c *Connections) SerializationJson(dataConnectsInFile string) {
 	err := json.Unmarshal([]byte(dataConnectsInFile), &c)
@@ -35,40 +21,4 @@ func (c *Connections) deserializationJson() []byte {
 	}
 
 	return newDataConnect
-}
-
-func (c *Connections) WriteConnectToJson(connect Connect) {
-	filePath, err := file.GetFullPath(os.Getenv("FILE_NAME_CONNECTS"))
-	if err != nil {
-		output.GetOutError("Ошибка получения путя к файлу")
-	}
-
-	c.SerializationJson(file.ReadFile(filePath))
-
-	encodedConnect := SetCryptData(connect)
-
-	c.Connects = append(c.Connects, encodedConnect)
-
-	file.WriteFile(getPathToConnectFile(), c.deserializationJson())
-}
-
-func (c *Connections) DeleteConnectToJson(alias string) {
-	filePath, err := file.GetFullPath(os.Getenv("FILE_NAME_CONNECTS"))
-	if err != nil {
-		output.GetOutError("Ошибка получения путя к файлу")
-	}
-
-	c.SerializationJson(file.ReadFile(filePath))
-
-	for i, v := range c.Connects {
-		if v.Alias == alias {
-			c.deleteJsonDataByIndex(i)
-
-			file.WriteFile(getPathToConnectFile(), c.deserializationJson())
-
-			return
-		}
-	}
-
-	output.GetOutError("Не найдено подключение")
 }
