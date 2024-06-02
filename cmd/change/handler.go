41932b6ef2f00abfef3839fe92d41de921f401e3
deniskorbakov <korbakovd@gmail.com>
2024-06-02 13:39:02 +0300

    feat: create handler for cmd update

---
+++ cmd/change/handler.go
@@ -0,0 +1,31 @@
+package change
+
+import (
+	"ssh+/app/json"
+	"ssh+/app/output"
+)
+
+var connects json.Connections
+
+func Connect(
+	oldAlias string,
+	alias, address, login, password string) {
+	var connections json.Connections
+
+	connect := json.Connect{
+		Alias:    alias,
+		Address:  address,
+		Login:    login,
+		Password: password,
+	}
+
+	connections.UpdateConnectJson(oldAlias, connect)
+}
+
+func ExistByIndex(alias string) {
+	_, err := connects.ExistConnectJsonByIndex(alias)
+
+	if err != nil {
+		output.GetOutError("Не найдено подключение")
+	}
+}
