330b227cccaeacd28447e4bbe2dc28d819fff560
deniskorbakov <korbakovd@gmail.com>
2024-06-16 12:55:59 +0300

    feat: delete extra code

--- inits/inits.go
+++ inits/inits.go
@@ -1,39 +1,25 @@
 package inits
 
 import (
-	"github.com/spf13/viper"
 	"ssh+/app/crypt"
-	"ssh+/app/file"
 	"ssh+/app/json"
 	"ssh+/config"
-
-	"github.com/joho/godotenv"
 )
 
-func loadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+func generateConfigFile() {
+	config.Generate()
+}
+
+func createFileConnects() {
+	json.Generate()
 }
 
 func generateCryptKey() {
 	crypt.GenerateKey()
 }
 
-func generateConfigFile() {
-	config.Generate()
-
-}
-
-func createFileConnects() {
-	file.GenerateFile(viper.GetString("NameFileConnects"))
-	json.CreateBaseJsonDataToFile()
-}
-
 func SetDependencies() {
 	generateConfigFile()
-	loadEnv()
-	generateCryptKey()
 	createFileConnects()
+	generateCryptKey()
 }
