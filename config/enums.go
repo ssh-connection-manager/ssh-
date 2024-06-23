07caca265b2f08585f0b3a85c5f8a07a7e618aab
deniskorbakov <korbakovd@gmail.com>
2024-06-23 18:29:11 +0300

    fix: delete and update order const

--- config/enums.go
+++ config/enums.go
@@ -1,25 +1,19 @@
 package config
 
 const (
-	FullPathConfig = "/etc/ssh+/"
+	DirectionApp = "/ssh+/"
 )
 
 const (
-	NameFileConnects = "connect-ssh.json"
-	NameFileCryptKey = "key.txt"
-)
-
-const (
-	Space     = " "
-	Separator = "/"
-	Dot       = "."
-)
-
-const (
-	IdRootUser = 0
-
 	NameFileConfig = "config"
 	TypeFileConfig = "yml"
+	Separator      = "/"
+	Space          = " "
+	Dot            = "."
+)
 
+const (
 	FullNameFileConfig = NameFileConfig + Dot + TypeFileConfig
+	NameFileConnects   = "connect-ssh.json"
+	NameFileCryptKey   = "key.txt"
 )
