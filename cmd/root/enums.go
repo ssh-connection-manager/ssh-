5b054fea920650c3a0ea9676ed0a88da00d2b61e
deniskorbakov <korbakovd@gmail.com>
2024-06-24 00:11:45 +0300

    feat: add chars for get in root command

--- cmd/root/enums.go
+++ cmd/root/enums.go
@@ -3,10 +3,14 @@ package root
 const (
 	UseCommand       = `ssh+`
 	ShortDescription = `A brief description of your application`
-	LongDescription  = `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	LongDescription  = `
+░░██████╗░██████╗██╗░░██╗░░░░░░░░
+░██╔════╝██╔════╝██║░░██║░░██╗░░░
+░╚█████╗░╚█████╗░███████║██████╗░
+░░╚═══██╗░╚═══██╗██╔══██║╚═██╔═╝░
+░██████╔╝██████╔╝██║░░██║░░╚═╝░░░
+░╚═════╝░╚═════╝░╚═╝░░╚═╝░░░░░░░░
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`
+`
+	DescriptionVersionApp = "version: "
 )
