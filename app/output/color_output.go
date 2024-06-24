2a6ba9f82331ce6187b444edee39b65a0a359a1b
deniskorbakov <korbakovd@gmail.com>
2024-06-24 23:26:42 +0300

    fix: update logic out

--- app/output/color_output.go
+++ app/output/color_output.go
@@ -1,19 +1,34 @@
 package output
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/fatih/color"
 )
 
+func exitApp() {
+	fmt.Println("error output")
+	os.Exit(1)
+}
+
+func getOut(color *color.Color, message string) {
+	_, err := color.Println(message)
+	if err != nil {
+		exitApp()
+	}
+
+	os.Exit(1)
+}
+
 func GetOutSuccess(successName string) {
 	green := color.New(color.BgGreen)
-	green.Println(successName)
+
+	getOut(green, successName)
 }
 
 func GetOutError(errName string) {
 	red := color.New(color.FgRed)
-	red.Println(errName)
 
-	os.Exit(1)
+	getOut(red, errName)
 }
