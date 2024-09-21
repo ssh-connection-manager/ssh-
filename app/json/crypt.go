c3473b9929f842b0f8076b4bbef9559a3e99e666
deniskorbakov <korbakovd@gmail.com>
2024-09-21 14:57:43 +0300

    feat: add base logic with new crypt module

--- app/json/crypt.go
+++ app/json/crypt.go
@@ -1,29 +1,47 @@
 package json
 
 import (
-	"ssh+/app/crypt"
+	"github.com/spf13/viper"
+	"github.com/ssh-connection-manager/crypt"
+	"ssh+/app/output"
 )
 
-func SetCryptData(c Connect) Connect {
-	c.Alias = crypt.Encrypt(c.Alias)
-	c.Address = crypt.Encrypt(c.Address)
-	c.Login = crypt.Encrypt(c.Login)
-	c.Password = crypt.Encrypt(c.Password)
+// TODO сделать через цикл чтобы шифровал каждое значение и так же правильн обрабатывал ошибки
 
-	c.CreatedAt = crypt.Encrypt(c.CreatedAt)
-	c.UpdatedAt = crypt.Encrypt(c.UpdatedAt)
+func SetCryptData(c Connect) Connect {
+	var err error
+
+	pathConf := viper.GetString("FullPathConfig")
+	fileNameKey := viper.GetString("NameFileCryptKey")
+
+	c.Alias, err = crypt.Encrypt(c.Alias, pathConf, fileNameKey)
+	c.Address, err = crypt.Encrypt(c.Address, pathConf, fileNameKey)
+	c.Login, err = crypt.Encrypt(c.Login, pathConf, fileNameKey)
+	c.Password, err = crypt.Encrypt(c.Password, pathConf, fileNameKey)
+
+	c.CreatedAt, err = crypt.Encrypt(c.CreatedAt, pathConf, fileNameKey)
+	c.UpdatedAt, err = crypt.Encrypt(c.UpdatedAt, pathConf, fileNameKey)
+
+	if err != nil {
+		output.GetOutError("err crypt data")
+	}
 
 	return c
 }
 
-func decryptData(c Connect) Connect {
-	c.Alias = crypt.Decrypt(c.Alias)
-	c.Address = crypt.Decrypt(c.Address)
-	c.Login = crypt.Decrypt(c.Login)
-	c.Password = crypt.Decrypt(c.Password)
+// TODO сделать через цикл чтобы шифровал каждое значение и так же правильн обрабатывал ошибки
 
-	c.CreatedAt = crypt.Decrypt(c.CreatedAt)
-	c.UpdatedAt = crypt.Decrypt(c.UpdatedAt)
+func decryptData(c Connect) Connect {
+	pathConf := viper.GetString("FullPathConfig")
+	fileNameKey := viper.GetString("NameFileCryptKey")
+
+	c.Alias, _ = crypt.Decrypt(c.Alias, pathConf, fileNameKey)
+	c.Address, _ = crypt.Decrypt(c.Address, pathConf, fileNameKey)
+	c.Login, _ = crypt.Decrypt(c.Login, pathConf, fileNameKey)
+	c.Password, _ = crypt.Decrypt(c.Password, pathConf, fileNameKey)
+
+	c.CreatedAt, _ = crypt.Decrypt(c.CreatedAt, pathConf, fileNameKey)
+	c.UpdatedAt, _ = crypt.Decrypt(c.UpdatedAt, pathConf, fileNameKey)
 
 	return c
 }
